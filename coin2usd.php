b8edd5d8390091f02e5c5d7a5c033a03831c75d2
sam <apple@remonte.local>
2018-11-22 21:42:56 +0800

    Coins/Token to USD

---
+++ coin2usd.php
@@ -0,0 +1,55 @@
+<style>
+#coineru-converter,
+#other-curr{
+	display: flex;
+	justify-content: space-evenly;
+}
+</style>
+<div id="coineru-converter">
+	  <div class="symbol symbol-eru">
+				<div class="eru">
+						<p><span>ERU</span> to </p>
+				</div>
+		</div>
+		<div class="symbol symbol-all">
+				<select name="coins" id="coins">
+				    <option value="BTC">Bitcoin</option>
+				    <option value="ETH">Ethereum</option>
+				    <option value="XRP">XRP</option>
+						<option value="EOS">EOS</option>
+						<option value="XLM">Stellar Lumens</option>
+						<option value="TRX">TRON</option>
+				    <option value="BCHABC">Bitcoin Cash ABC</option>
+				    <option value="TUSD">TrueUSD</option>
+						<option value="NEO">NEO</option>
+						<option value="ADA">Cardano</option>
+						<option value="LTC">Litecoin</option>
+						<option value="BCHSV">Bitcoin Cash SV</option>
+						<option value="ETC">Ethereum Classic</option>
+						<option value="ONT">Onthology</option>
+				    <option value="PAX">Paxos Standard</option>
+				    <option value="BNB">Binance Coin</option>
+						<option value="IOTA">MIOTA</option>
+				    <option value="VET">VeChain</option>
+				    <option value="ICX">ICON</option>
+				    <option value="QTUM">Qtum</option>
+						<option value="NULS">Nuls</option>
+				    <option value="ZEC">Zcash</option>
+				    <option value="XTZ">Tezos</option>
+				    <option value="ZRX">0x</option>
+				</select>
+		</div>
+		<div class="symbol">
+			  <div class="">
+						<p>$ <span class="converted"></span></p>
+				</div>
+		</div>
+</div>
+<div id="other-curr">
+		<div class="jpy">
+				<p>Y <span id="jpy"></span></p>
+		</div>
+		<div class="eur">
+				<p>E <span id="eur"></span></p>
+		</div>
+</div>
---
+++ coins2usd.js
@@ -0,0 +1,22 @@
+(function ($){
+    $.getCoinValue = function(token){
+        $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=' +token+ '&tsyms=USD,JPY,EUR', function(data){
+            console.log(data.USD);
+            $('.converted').text(data.USD);
+            $('#jpy').text(data.JPY);
+            $('#eur').text(data.EUR);
+        });
+    }
+})(jQuery);
+
+jQuery(document).ready(function($){
+    $(window).on('load', function(){
+        $.getCoinValue('BTC');
+    });
+    $('#coins').change(function(e){
+        e.preventDefault();
+        const tkn = $(this).val();
+        $.getCoinValue(tkn);
+        console.log(tkn);
+    });
+});
